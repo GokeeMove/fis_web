743ac6da3135ff6ffbb91f08326128193c7a9ab6
gokeemove <gokeemove@gmail.com>
2018-09-27 10:13:54 +0800

    new wx

--- python/WxList.py
+++ python/WxList.py
@@ -6,38 +6,34 @@ from urllib.request import urlopen
 import re
 
 
-def getText(base_url):
-    content = urlopen(base_url)
+def getText(base_url, til_url):
+    content = urlopen(base_url + til_url)
     bsObj = BeautifulSoup(content, "lxml")
     i = 0
     record = False
     for dd in bsObj.findAll('dd'):
         for link in dd.findAll('a'):
             if 'href' in link.attrs:
-                # if (re.search(r'\d+.html', link.attrs['href']) \
-                #             and re.search(r'^[\d+_\d+.html]', link.attrs['href']) \
-                #     ):
-                    list = re.findall(r'\d+', link.get_text())
-                    if link.get_text() == '第二十六章 审时度势 广源剑修':
-                        record = True
-                    if record:
-                        print(link.get_text())
-                        print(link.attrs['href'])
-                        getContent("https://www.zwdu.com" + link.attrs['href'])
-                        # i + 1
-                # else:
-                #     print("last")
-                    # print("page->" + i)
+                if link.get_text() == '第一百四十七章 大难不死必有后福':
+                    record = True
+                else:
+                    print("skip->" + link.get_text())
+
+                if record:
+                    print("output->" + link.get_text())
+                    print(link.attrs['href'])
+                    getContent(base_url + link.attrs['href'])
+                    i + 1
 
 
 def getContent(url):
     content = urlopen(url)
     bsObj = BeautifulSoup(content, "lxml")
     title = str(bsObj.title.get_text())
-    tfile = open("dadaozhengfeng.txt", 'a')
+    tfile = open("/Users/libowen/Desktop/" + title.split("_")[1] + ".txt", 'a')
     tfile.write("\n\n\n" + str(bsObj.title.get_text()) + "\n")
     tfile.write(bsObj.find_all("div", {"id": "content"})[0].get_text())
     tfile.close()
 
 
-getText("https://www.zwdu.com/book/8017/")
+getText("https://www.biquge.com.tw", "/6_6967/")
